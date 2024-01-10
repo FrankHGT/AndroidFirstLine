29191f8a9d727fbd44648b6e08dd4e9a1348c1a1
Frank Hu <huguantingfrank@outlook.com>
2024-01-10 11:29:00 +0800

    ch5: 动态添加/修改fragment

--- ch5/FragmentTest/app/src/main/java/com/example/fragmenttest/LeftFragment.kt
+++ ch5/FragmentTest/app/src/main/java/com/example/fragmenttest/LeftFragment.kt
@@ -1,12 +1,18 @@
 package com.example.fragmenttest
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fragmenttest.databinding.LeftFragmentBinding
 
 class LeftFragment : Fragment() {
+
+    companion object {
+        const val TAG = "LeftFragment"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -14,6 +20,16 @@ class LeftFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.left_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = LeftFragmentBinding.bind(view)
+        binding.button.setOnClickListener {
+            Log.d(TAG, "button clicked.")
+            (activity as MainActivity).replaceFragment(AnotherRightFragment())
+        }
     }
 
 }
--- ch5/FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.kt
+++ ch5/FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.fragmenttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.example.fragmenttest.databinding.ActivityMainBinding
+import com.example.fragmenttest.databinding.LeftFragmentBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 为什么这里的button onClick不会回调
+        // GPT回答：
+        // 在onCreate方法中，你通过LeftFragmentBinding.inflate(layoutInflater)创建了一个LeftFragment的实例
+        // （实例A），并为其内部的按钮设置了点击监听。然而，这个Fragment的实例并没有被添加到你的Activity中，因此按钮
+        // 的点击事件也就无法被捕获。
+        // 同时在XML布局文件中，通过<fragment android:name="com.example.fragmenttest.LeftFragment" />这行
+        // 代码，你又创建了另一个LeftFragment的实例（实例B）并添加到了Activity中。虽然实例B已经添加到Activity并可
+        // 见，但其按钮并未设置点击监听。
+        // 简而言之，你的按钮监听被设置在了实例A的按钮上，但实际上你看到并且进行操作的却是实例B的按钮，这就导致了你所描
+        // 述的问题。
+//        val leftFragmentBinding = LeftFragmentBinding.inflate(layoutInflater)
+//        leftFragmentBinding.button.setOnClickListener {
+//            Log.d(TAG, "button clicked.")
+//            replaceFragment(AnotherRightFragment())
+//        }
+        replaceFragment(RightFragment())
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.rightLayout, fragment)
+        transaction.commit()
     }
 }
