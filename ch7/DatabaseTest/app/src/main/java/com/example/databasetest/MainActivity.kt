4d85d0564d293b5f8ca11578b1275e4f2fa45202
Frank Hu <huguantingfrank@outlook.com>
2024-01-12 18:22:36 +0800

    ch7: 数据库建立，新增数据

---
+++ ch7/DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.databasetest
+
+import android.content.ContentValues
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.databasetest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val dbHelper = MyDatabaseHelper(this, "BookStore.db", 2)
+        binding.createDatabase.setOnClickListener {
+            dbHelper.writableDatabase
+        }
+        binding.addData.setOnClickListener {
+            val db = dbHelper.writableDatabase
+            val values1 = ContentValues().apply {
+                put("name", "The Da Vinci Code")
+                put("author", "Dan Brown")
+                put("pages", 454)
+                put("price", 16.96)
+            }
+            db.insert("Book", null, values1)
+            val values2 = ContentValues().apply {
+                put("name", "The Lost Symbol")
+                put("author", "Dan Brown")
+                put("pages", 510)
+                put("price", 19.95)
+            }
+            db.insert("Book", null, values2)
+        }
+    }
+}
