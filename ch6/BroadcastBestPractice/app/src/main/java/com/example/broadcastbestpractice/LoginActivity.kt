d50bad717df63745505176f5d35f20ea9a8d7344
Frank Hu <huguantingfrank@outlook.com>
2024-01-12 16:12:52 +0800

    ch7: 使用SharedPreferences技术，实现记住密码功能

--- ch6/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
+++ ch6/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.broadcastbestpractice
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,12 +14,33 @@ class LoginActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val prefs = getPreferences(Context.MODE_PRIVATE)
+        val isRemember = prefs.getBoolean("remember_password", false)
+        if (isRemember) {
+            binding.apply {
+                val account = prefs.getString("account", "")
+                val password = prefs.getString("password", "")
+                accountEdit.setText(account)
+                passwordEdit.setText(password)
+                rememberPassword.isChecked = true
+            }
+        }
         binding.apply {
             login.setOnClickListener {
                 val account = accountEdit.text.toString()
                 val password = passwordEdit.text.toString()
 
                 if (account == "frank" && password == "111111") {
+                    val editor = prefs.edit()
+                    if (rememberPassword.isChecked) {
+                        editor.putBoolean("remember_password", true)
+                        editor.putString("account", account)
+                        editor.putString("password", password)
+
+                    } else {
+                        editor.clear()
+                    }
+                    editor.apply()
                     val intent = Intent(this@LoginActivity, MainActivity::class.java)
                     startActivity(intent)
                     finish()
