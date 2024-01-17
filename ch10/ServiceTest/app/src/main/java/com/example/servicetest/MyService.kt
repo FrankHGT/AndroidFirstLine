c1e3f8ed36b7d197b0b4b6739c25ad9f5573da49
Frank Hu <huguantingfrank@outlook.com>
2024-01-17 20:55:21 +0800

    ch10: 前台服务

--- ch10/ServiceTest/app/src/main/java/com/example/servicetest/MyService.kt
+++ ch10/ServiceTest/app/src/main/java/com/example/servicetest/MyService.kt
@@ -1,10 +1,17 @@
 package com.example.servicetest
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class MyService : Service() {
 
@@ -30,6 +37,22 @@ class MyService : Service() {
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG, "onCreate executed")
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("my_service", "前台Service通知",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+        }
+        val intent = Intent(this, MainActivity::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val notification = NotificationCompat.Builder(this, "my_service")
+            .setContentTitle("This is content title")
+            .setContentText("This is content text")
+            .setSmallIcon(androidx.appcompat.R.drawable.abc_btn_radio_material)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, androidx.constraintlayout.widget.R.drawable.abc_btn_radio_material))
+            .setContentIntent(pi)
+            .build()
+        startForeground(1, notification)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
