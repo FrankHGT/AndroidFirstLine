d7a6bdf17feb08bc48c8095a9409e8e1645cc66d
Frank Hu <huguantingfrank@outlook.com>
2024-01-17 20:37:23 +0800

    ch10: service两种启动方式，service生命周期

---
+++ ch10/ServiceTest/app/src/main/java/com/example/servicetest/MyService.kt
@@ -0,0 +1,48 @@
+package com.example.servicetest
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.util.Log
+
+class MyService : Service() {
+
+    private val mBinder = DownloadBinder()
+
+    class DownloadBinder : Binder() {
+
+        fun startDownload() {
+            Log.d(TAG, "startDownload executed")
+        }
+
+        fun getProgress(): Int {
+            Log.d(TAG, "getProgress executed")
+            return 0
+        }
+
+    }
+
+    companion object {
+        private const val TAG = "MyService"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "onCreate executed")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d(TAG, "onStartCommand executed")
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy executed")
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return mBinder
+    }
+}
