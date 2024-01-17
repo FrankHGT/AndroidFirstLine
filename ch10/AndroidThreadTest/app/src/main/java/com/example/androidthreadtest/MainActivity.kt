71dfbdb8898f08707f187c10b8452d940887577d
Frank Hu <huguantingfrank@outlook.com>
2024-01-17 20:36:38 +0800

    ch10: 子线程执行耗时操作，通过handler更新ui

--- ch10/AndroidThreadTest/app/src/main/java/com/example/androidthreadtest/MainActivity.kt
+++ ch10/AndroidThreadTest/app/src/main/java/com/example/androidthreadtest/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.androidthreadtest
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
+import android.widget.Toast
 import com.example.androidthreadtest.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
 
@@ -12,15 +14,81 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    val updateText = 1
+    val resetProcessBar = 2
+    val handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                updateText -> binding.textView.text = "Nice to meet you"
+            }
+        }
+    }
+
+    lateinit var downloadTask: DownloadTask
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.changeTextBtn.setOnClickListener {
             thread {
-                // FIXME: 子线程更新UI会导致崩溃
-                binding.textView.text = "Nice to meet you"
+                val msg = Message()
+                msg.what = updateText
+                handler.sendMessage(msg)
             }
         }
+        downloadTask = DownloadTask()
+        binding.downloadBtn.setOnClickListener {
+            downloadTask.execute()
+        }
     }
+
+    inner class DownloadTask : AsyncTask<Unit, Int, Boolean>() {
+
+        var progress = 0
+        override fun onPreExecute() {
+            binding.progressBar.progress = 0
+        }
+
+        override fun doInBackground(vararg params: Unit?) = try {
+            while (true) {
+                val downloadPercent = doDownload()
+                publishProgress(downloadPercent)
+                if (downloadPercent >= 100) {
+                    break
+                }
+            }
+            true
+        } catch (e: Exception) {
+            false
+        }
+
+        override fun onProgressUpdate(vararg values: Int?) {
+            binding.progressBar.progress = values[0] ?: 0
+        }
+
+        override fun onPostExecute(result: Boolean?) {
+            if (result == true) {
+                Toast.makeText(applicationContext, "Download succeeded", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Download failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        private fun doDownload(): Int {
+            progress += 10
+            Thread.sleep(500)
+            return progress
+            // 非ui线程访问ui也会有问题
+//            return binding.progressBar.progress + 10
+            // 这样也不行
+//            var res = 0
+//            runOnUiThread {
+//                res = binding.progressBar.progress + 10
+//            }
+//            return res
+        }
+
+    }
+
 }
