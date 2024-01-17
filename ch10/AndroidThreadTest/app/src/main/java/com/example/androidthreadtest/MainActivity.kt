f5df4dd17f584cecda9d7d1fd09d2dba5bd24718
Frank Hu <huguantingfrank@outlook.com>
2024-01-17 10:55:39 +0800

    ch10: 多线程-子线程不允许直接更新UI，否则会导致崩溃

---
+++ ch10/AndroidThreadTest/app/src/main/java/com/example/androidthreadtest/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.androidthreadtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import com.example.androidthreadtest.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.changeTextBtn.setOnClickListener {
+            thread {
+                // FIXME: 子线程更新UI会导致崩溃
+                binding.textView.text = "Nice to meet you"
+            }
+        }
+    }
+}
