2e6c97da0a0e883728066b21cd6f5060b5e0c605
Frank Hu <huguantingfrank@outlook.com>
2024-01-18 20:05:42 +0800

    ch10: parse xml with XmlPullParser

--- ch11/NetworkTest/app/src/main/java/com/example/networktest/MainActivity.kt
+++ ch11/NetworkTest/app/src/main/java/com/example/networktest/MainActivity.kt
@@ -1,25 +1,47 @@
 package com.example.networktest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.telephony.TelephonyManager
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.networktest.databinding.ActivityMainBinding
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.io.StringReader
 import java.net.HttpURLConnection
 import java.net.URL
 import kotlin.concurrent.thread
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val TAG = "MainActivity"
+
+
+        // 使用模拟器运行时，此ip地址可以访问宿主机
+//        private const val simulatorIP = "10.0.2.2"
+        private const val simulatorIP = "10.67.165.12"
+        private const val XML_URL = "http://$simulatorIP/get_data.xml"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.sendRequestBtn.setOnClickListener {
+            sendRequestWithHttpURLConnection()
+        }
+        binding.getXMLWhenAVDBtn.setOnClickListener {
             sendRequestWithOkHttp()
         }
     }
@@ -27,18 +49,60 @@ class MainActivity : AppCompatActivity() {
     private fun sendRequestWithOkHttp() {
         thread {
             try {
+//                if (!isEmulator(this)) {
+//                    runOnUiThread {
+//                        // !!! Toast也属于UI更新，也要在主线程执行
+//                        Toast.makeText(this, "not avd, can't access 10.0.2.2", Toast.LENGTH_SHORT).show()
+//                    }
+//                    return@thread
+//                }
                 val client = OkHttpClient()
                 val request = Request.Builder()
-                    .url("https://www.baidu.com")
+                    .url(XML_URL)
                     .build()
                 val response = client.newCall(request).execute()
                 val responseData = response.body?.string()
                 if (responseData != null) {
+                    parseXMLWithPull(responseData)
                     showResponse(responseData)
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
             }
+        }
+    }
+
+    private fun parseXMLWithPull(xmlData: String) {
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            val xmlPullParser = factory.newPullParser()
+            xmlPullParser.setInput(StringReader(xmlData))
+            var eventType = xmlPullParser.eventType
+            var id = ""
+            var name = ""
+            var version = ""
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                val nodeName = xmlPullParser.name
+                when (eventType) {
+                    XmlPullParser.START_TAG -> {
+                        when (nodeName) {
+                            "id" -> id = xmlPullParser.nextText()
+                            "name" -> name = xmlPullParser.nextText()
+                            "version" -> version = xmlPullParser.nextText()
+                        }
+                    }
+                    XmlPullParser.END_TAG -> {
+                        if ("app" == nodeName) {
+                            Log.d(TAG, "id is $id")
+                            Log.d(TAG, "name is $name")
+                            Log.d(TAG, "version is $version")
+                        }
+                    }
+                }
+                eventType = xmlPullParser.next()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
@@ -72,4 +136,21 @@ class MainActivity : AppCompatActivity() {
             binding.responseText.text = response
         }
     }
+
+    fun isEmulator(context: Context): Boolean {
+        return (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.HARDWARE.contains("goldfish")
+                || Build.HARDWARE.contains("ranchu")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion") || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith(
+            "generic"
+        )) || "google_sdk" == Build.PRODUCT || (context.getSystemService(
+            TELEPHONY_SERVICE
+        ) as TelephonyManager).networkOperatorName.equals("Android", ignoreCase = true)
+    }
+
 }
