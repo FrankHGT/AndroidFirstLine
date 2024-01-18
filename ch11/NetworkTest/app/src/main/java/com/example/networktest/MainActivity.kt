d6204574bceee33b95a85e11cbe7d9b2b297d78b
Frank Hu <huguantingfrank@outlook.com>
2024-01-18 16:07:05 +0800

    ch11: 简单的webview demo以及利用HttpURLConnection进行http通信的demo

---
+++ ch11/NetworkTest/app/src/main/java/com/example/networktest/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.networktest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.networktest.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.sendRequestBtn.setOnClickListener {
+            sendRequestWithHttpURLConnection()
+        }
+    }
+
+    private fun sendRequestWithHttpURLConnection() {
+        thread {
+            var connection: HttpURLConnection? = null
+            try {
+                val response = StringBuilder()
+                val url = URL("https://www.baidu.com")
+                connection = url.openConnection() as HttpURLConnection
+                connection.connectTimeout = 8000
+                connection.readTimeout = 8000
+                val input = connection.inputStream
+                val reader = BufferedReader(InputStreamReader(input))
+                reader.use {
+                    reader.forEachLine {
+                        response.append(it)
+                    }
+                }
+                showResponse(response.toString())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                connection?.disconnect()
+            }
+        }
+    }
+
+    private fun showResponse(response: String) {
+        runOnUiThread {
+            binding.responseText.text = response
+        }
+    }
+}
